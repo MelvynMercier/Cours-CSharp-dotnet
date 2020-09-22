088f93ed6f8a4358c799ac61e613ccde5bac7be0
MelvynMercier <43468212+MelvynMercier@users.noreply.github.com>
2020-09-22 13:46:06 +0200

    Commit

--- Exercice 1/ArticleTableau/Program.cs
+++ Exercice 1/ArticleTableau/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ArticleTableau.Structures;
+using System;
+using System.Collections.Generic;
 
 namespace ArticleTableau
 {
@@ -6,7 +8,19 @@ namespace ArticleTableau
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var listArticle = new List<Article>();
+            listArticle.Add(new Article("Chemise", 15, 1, Structures.Type.Habillement));
+            listArticle.Add(new Article("JetSki", 15000, 6, Structures.Type.Loisir));
+            listArticle.Add(new Article("Bateau", 51000, 6, Structures.Type.Loisir));
+
+            listArticle.ForEach(a =>
+            {
+                Console.WriteLine(a.Nom);
+                Console.WriteLine(a.Prix);
+                Console.WriteLine(a.Quantite);
+                Console.WriteLine(a.TypeType);
+                Console.WriteLine("");
+            });
         }
     }
 }
---
+++ Exercice 1/ArticleTableau/Structures/Article.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArticleTableau.Structures
+{
+    public enum Type
+    {
+        Alimentaire, Droguerie, Habillement, Loisir
+    }
+
+    public class Article
+    {
+        public string Nom { get; set; }
+        public int? Prix { get; set; }
+        public int? Quantite { get; set; }
+        public Type TypeType { get; set; }
+
+        public Article(string Nom, int Prix, int Quantite, Type type)
+        {
+            this.Nom = Nom;
+            this.Prix = Prix;
+            this.Quantite = Quantite;
+            this.TypeType = type;
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine("Nom: " + this.Nom);
+            Console.WriteLine("Prix: " + this.Prix);
+            Console.WriteLine("Quantité: " + this.Quantite);
+        }
+
+        public void Ajouter(int Integer)
+        {
+            this.Prix = this.Prix + Integer;
+            Console.WriteLine("Prix: " + this.Prix);
+        }
+
+        public void Retirer(int Integer)
+        {
+            this.Prix = this.Prix - Integer;
+            Console.WriteLine("Prix: " + this.Prix);
+        }
+    }
+}
